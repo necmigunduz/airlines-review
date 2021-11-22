f89b7aecb72f83180718970bf7ac1857ba848395
necmigunduz <necmigunduz@gmail.com>
2021-11-22 18:05:28 +0300

    Add models

--- db/migrate/20211122150405_create_reviews.rb
+++ db/migrate/20211122150405_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :description
       t.integer :score
+      t.belongs_to :airline, null: false, foreign_key: true
 
       t.timestamps
     end
