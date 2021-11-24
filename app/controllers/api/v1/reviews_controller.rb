ad33d255bbfeb123fa23185438470b4a65e04d21
necmigunduz <necmigunduz@gmail.com>
2021-11-24 17:54:30 +0300

    Add airlines and reviews controllers

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,53 +1,30 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
-            def index
-                airlines = Airline.all
-
-                render json: AirlineSerializer.new(airlines).serialized_json
-            end
-
-            def show
-                airline = Airline.find_by(slug: params[:slug])
-
-                render json: AirlineSerializer.new(airline).serialized_json
-            end
-
             def create
-                airline = Airline.new(airline_params)
+                review = Review.new(review_params)
 
-                if airline.save
-                    render json: AirlineSerializer.new(airline).serialized_json
+                if review.save
+                    render json: ReviewSerializer.new(review).serialized_json
                 else
-                    render json: { error: airline.error.messages }, status: 422 
-                end
-            end
-
-            def update
-                airline = Airline.find_by(slug: params[:slug])
-
-                if airline.update(airline_params)
-                    render json: AirlineSerializer.new(airline).serialized_json
-                else
-                    render json: { error: airline.error.messages }, status: 422 
+                    render json: { error: review.errors.messages }, status: 422
                 end
             end
 
             def destroy
-                airline = Airline.find_by(slug: params[:slug])
+                review = Review.find(params[:id])
 
-                if airline.destroy
+                if review.destroy
                     head :no_content
                 else
-                    render json: { error: airline.error.messages }, status: 422 
+                    render json: { error: review.errors.messages }, status: 422
                 end
             end
 
             private
-            def airline_params
-                params.require(:airline).permit(:name, :image_url)
+            def review_params
+                params.require(:review).permit(:title, :description, :score, :airline_id)
             end
-
         end
     end
 end
